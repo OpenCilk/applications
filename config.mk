a82a4a48d8bdf373c84b2823ec2f34295a4ad530
John F. Carr <jfc@mit.edu>
2025-02-18 12:05:57 -0500

    Rename compiler bin to OPENCILK_BIN.  Use ?= instead of = in assignments.

--- config.mk
+++ config.mk
@@ -1,8 +1,12 @@
-OPENCILK_BUILD=
-CC=$(OPENCILK_BUILD)clang
-CXX=$(OPENCILK_BUILD)clang++
+OPENCILK_BIN?=
+CC=$(OPENCILK_BIN)clang
+CXX=$(OPENCILK_BIN)clang++
 CILKFLAG=-fopencilk
-OPT=-O2
-DBG=-g3
-ARCH=-mavx -march=sandybridge
+OPT?=-O2
+DBG?=-g3
+MACHINE?=$(shell uname -m)
+ARCH_x86_64=-march=x86-64-v3	# uname -m on Linux and Darwin
+ARCH_amd64=-march=x86-64-v3	# uname -m on FreeBSD
+ARCH_aarch64=
+ARCH=$(ARCH_$(MACHINE))
 TOOL=
